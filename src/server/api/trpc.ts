a9de7cc006e99831ce12446ed4d00fe5e24780ed
nisioka <16322296+nisioka@users.noreply.github.com>
2024-08-08 22:14:37 +0900

    add storybook.

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -11,6 +11,7 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { db } from "~/server/db";
+import {OpenApiMeta} from "trpc-openapi";
 
 /**
  * 1. CONTEXT
@@ -38,7 +39,7 @@ export const createTRPCContext = async (opts: { headers: Headers }) => {
  * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
  * errors on the backend.
  */
-const t = initTRPC.context<typeof createTRPCContext>().create({
+const t = initTRPC.meta<OpenApiMeta>().context<typeof createTRPCContext>().create({
   transformer: superjson,
   errorFormatter({ shape, error }) {
     return {
